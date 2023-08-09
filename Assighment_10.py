34ec8ffa69f0f9f577cceafeff3acda381b64f70
Fatih Kaya <67631361+kayafatix@users.noreply.github.com>
2023-08-09 16:54:11 +0200

    new project

---
+++ Assighment_10.py
@@ -0,0 +1,21 @@
+fhand = open("mbox-short.txt")
+counts = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+lst = list()
+
+for key, val in counts.items():
+    newtup = (val, key)
+    lst.append(newtup)
+
+lst = sorted(lst, reverse=True)
+
+for val, key in lst[:10]:
+    print(key, val)
+
+#
+# c= {"a": 1, "b": 12, "d": 33}
+# print(sorted([(v,k) for k,v in c.items()], reverse=True))
